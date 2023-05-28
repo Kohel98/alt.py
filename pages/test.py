dbd95f80d9459afdf73fd02a4b9eade437cf9bf6
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-05-27 22:54:03 -0600

    sympy1

---
+++ pages/test.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import sympy as sy
+
+tab1, tab2, tab3 = st.tabs(["Definiciones","Ejemplos","Aplicaciones"])
+with tab1:
+     st.title(":blue[Descenso de la Gradiente]")
