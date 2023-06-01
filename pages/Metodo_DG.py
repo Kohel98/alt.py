23de650d4945a15d386fe93ed9651a6b705f5c17
Kohel98 <94484430+Kohel98@users.noreply.github.com>
2023-06-01 07:38:39 -0600

    aver

--- pages/Metodo_DG.py
+++ pages/Metodo_DG.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import functions.sfunctions as sf
 
 tab1, tab2, tab3 = st.tabs(["Definiciones","Ejemplos","Aplicaciones"])
 with tab1:
